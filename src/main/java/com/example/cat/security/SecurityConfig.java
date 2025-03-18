4cdb8bf6aa0a9a126c7f0a512291f633b9e70c9e
Yunona <guzel.ru.2015@mail.ru>
2025-03-18 18:54:28 +0500

    upd annotation

--- src/main/java/com/example/cat/security/SecurityConfig.java
+++ src/main/java/com/example/cat/security/SecurityConfig.java
@@ -2,12 +2,13 @@ package com.example.cat.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableGlobalMethodSecurity (prePostEnabled = true)
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
